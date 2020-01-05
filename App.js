2290c21111cb8fce0e01cc29e4483e0df6cad028
dikimeip <pm159145@gmail.com>
2020-01-05 09:20:55 +0700

    membuat blok untuk mengatur lokasi menu

--- App.js
+++ App.js
@@ -17,32 +17,46 @@ import {
 
 const App = () => {
   return (
-    <View style={{ backgroundColor: 'pink', flex: 1 }}>
+    <View style={{ flex: 1 }}>
       {/* menu konten */}
-      <View style={{ backgroundColor: 'brown', flex: 1 }}></View>
+      <View style={{ flex: 1 }}>
+        {/* Image Slider */}
+        <View style={{ backgroundColor: 'yellow', height: 200 }}></View>
+        {/* menu fiture */}
+        <View style={{ backgroundColor: 'green', height: 200, flexDirection: 'row', flexWrap: 'wrap' }}>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'white', height: 70,  width: '25%' }}></View>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'black', height: 70,  width: '25%' }}></View>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'pink', height: 70,  width: '25%' }}></View>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'red', height: 70,  width: '25%' }}></View>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'white', height: 70,  width: '25%' }}></View>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'black', height: 70,  width: '25%' }}></View>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'pink', height: 70,  width: '25%' }}></View>
+          <View style={{marginTop: 20,alignItems:'center',justifyContent:'center', backgroundColor: 'red', height: 70,  width: '25%' }}></View>
+        </View>
+      </View>
       {/* menu bawah */}
       <View style={{ backgroundColor: 'white', height: 70, flexDirection: 'row', }}>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <View style={{ width: 30, height: 30 }}>
-           <Image source={require('./src/assets/icon/home.png')} style={{width:'100%',height:'100%'}} />
+          <View style={{ width: 26, height: 26 }}>
+            <Image source={require('./src/assets/icon/home.png')} style={{ width: '100%', height: '100%' }} />
           </View>
           <Text style={{ color: 'black', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>DASBOARD</Text>
         </View>
-        <View style={{ flex: 1 ,alignItems: 'center', justifyContent: 'center' }}>
-          <View style={{ width: 30, height: 30 }}>
-          <Image source={require('./src/assets/icon/news.png')} style={{width:'100%',height:'100%'}} />
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ width: 26, height: 26 }}>
+            <Image source={require('./src/assets/icon/news.png')} style={{ width: '100%', height: '100%' }} />
           </View>
           <Text style={{ color: 'black', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>BERITA</Text>
         </View>
-        <View style={{  flex: 1 ,alignItems: 'center', justifyContent: 'center' }}>
-          <View style={{ width: 30, height: 30 }}>
-          <Image source={require('./src/assets/icon/report.png')} style={{width:'100%',height:'100%'}} />
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ width: 26, height: 26 }}>
+            <Image source={require('./src/assets/icon/report.png')} style={{ width: '100%', height: '100%' }} />
           </View>
           <Text style={{ color: 'black', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>PENGADUAN</Text>
         </View>
-        <View style={{ flex: 1 ,alignItems: 'center', justifyContent: 'center' }}>
-          <View style={{ width: 30, height: 30 }}>
-          <Image source={require('./src/assets/icon/contact.png')} style={{width:'100%',height:'100%'}} />
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ width: 26, height: 26 }}>
+            <Image source={require('./src/assets/icon/contact.png')} style={{ width: '100%', height: '100%' }} />
           </View>
           <Text style={{ color: 'black', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>TENTANG</Text>
         </View>

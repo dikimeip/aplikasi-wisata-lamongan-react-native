9b5e6261af78b2b1a09a236fdc69d2e93f63aab4
dikimeip <pm159145@gmail.com>
2020-01-05 08:30:53 +0700

    melakukan desain kasar untuk footbar

--- App.js
+++ App.js
@@ -16,14 +16,34 @@ import {
 
 const App = () => {
   return (
-    <View>
-        <Text>HELLO WORD</Text>
+    <View style={{ backgroundColor: 'pink', flex: 1 }}>
+      {/* menu konten */}
+      <View style={{ backgroundColor: 'brown', flex: 1 }}></View>
+      {/* menu bawah */}
+      <View style={{ backgroundColor: 'blue', height: 70, flexDirection: 'row', }}>
+        <View style={{ backgroundColor: 'white', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ backgroundColor: 'pink', width: 30, height: 30 }}></View>
+          <Text style={{ color: 'green', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>DASBOARD</Text>
+        </View>
+        <View style={{ backgroundColor: 'green', flex: 1 ,alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ backgroundColor: 'pink', width: 30, height: 30 }}></View>
+          <Text style={{ color: 'green', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>DASBOARD</Text>
+        </View>
+        <View style={{ backgroundColor: 'black', flex: 1 ,alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ backgroundColor: 'pink', width: 30, height: 30 }}></View>
+          <Text style={{ color: 'green', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>DASBOARD</Text>
+        </View>
+        <View style={{ backgroundColor: 'yellow', flex: 1 ,alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ backgroundColor: 'pink', width: 30, height: 30 }}></View>
+          <Text style={{ color: 'green', fontSize: 14, fontWeight: 'bold', marginTop: 10 }}>DASBOARD</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  
+
 });
 
 export default App;

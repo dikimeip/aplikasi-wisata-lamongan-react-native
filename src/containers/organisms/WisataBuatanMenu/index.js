3946c1303f1e9a981218947d630ec9326f1bb589
dikimeip <pm159145@gmail.com>
2020-01-08 11:06:53 +0700

    menampilkan data dari backend di page wisata buatan

--- src/containers/organisms/WisataBuatanMenu/index.js
+++ src/containers/organisms/WisataBuatanMenu/index.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import { View,Text,ScrollView,Image } from 'react-native';
+import { View, Text, ScrollView, FlatList } from 'react-native';
 import WisataItem from '../../../components/moleculs/WisataItem';
+import Axios from 'axios'
 
-const WisataBuatanMenu = () => {
-    return (
-        <View style={{ height: 210, marginTop: 20 }}>
-            <Text style={{ fontWeight: 'bold', fontSize: 20, borderBottomWidth: 1, borderBottomColor: 'grey' }}>WISATA BUATAN</Text>
-            <ScrollView horizontal>
-                <View style={{ flexDirection: 'row' }}>
-                    <WisataItem nama="Dummy Picture" foto={require('../../../assets/foto/m1.jpg')} />
-                    <WisataItem nama="Dummy Picture" foto={require('../../../assets/foto/m1.jpg')} />
-                    <WisataItem nama="Dummy Picture" foto={require('../../../assets/foto/m1.jpg')} />
-                    <WisataItem nama="Dummy Picture" foto={require('../../../assets/foto/m1.jpg')} />
-                </View>
-            </ScrollView>
-        </View>
+class WisataBuatanMenu extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            prefik_url :"http://192.168.43.230/api_pariwisata/img/",
+            wisata : []
+        }
+    }
+
+    componentDidMount = () => {
+        Axios.get("http://192.168.43.230/api_pariwisata/show_wisatabuatan.php").then(res => {
+            this.setState({
+                wisata: res.data.data
+            })
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    keyExtractor = (item, index) => index.toString()
+    renderItem = ({ item }) => (
+        <WisataItem nama={item.nama_wisata} foto={{ uri: this.state.prefik_url + item.foto1 }} />
     )
+
+    render() {
+        return (
+            <View style={{ height: 210, marginTop: 20 }}>
+                <Text style={{ fontWeight: 'bold', fontSize: 20, borderBottomWidth: 1, borderBottomColor: 'grey' }}>WISATA BUATAN</Text>
+                <FlatList
+                    keyExtractor={this.keyExtractor}
+                    data={this.state.wisata}
+                    renderItem={this.renderItem}
+                    horizontal={true}
+                />
+            </View>
+        )
+    }
+
 }
 
 export default WisataBuatanMenu

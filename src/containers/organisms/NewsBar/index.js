b2295ab31af65c9cb872cf244b24c5d47a0488fb
dikimeip <pm159145@gmail.com>
2020-01-08 11:28:28 +0700

    menampilkan data dari backend di page berita

--- src/containers/organisms/NewsBar/index.js
+++ src/containers/organisms/NewsBar/index.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import { ScrollView,View } from 'react-native';
+import { ScrollView, View ,FlatList} from 'react-native';
 import NewsItem from '../../../components/moleculs/NewsItem';
+import Axios from 'axios'
 
-const NewsBar = () => {
-    return (
-        <View style={{ flex: 1 }}>
-            {/* berita content */}
-            <ScrollView>
-                <NewsItem nama="Dumy Foto" foto={require('../../../assets/foto/m1.jpg')} />
-                <NewsItem nama="Dumy Foto" foto={require('../../../assets/foto/m1.jpg')} />
-                <NewsItem nama="Dumy Foto" foto={require('../../../assets/foto/m1.jpg')} />
-                <NewsItem nama="Dumy Foto" foto={require('../../../assets/foto/m1.jpg')} />
-                <NewsItem nama="Dumy Foto" foto={require('../../../assets/foto/m1.jpg')} />
-            </ScrollView>
-        </View>
+class NewsBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            prefik_url :"http://192.168.43.230/api_pariwisata/img/",
+            wisata : []
+        }
+    }
+
+    componentDidMount = () => {
+        Axios.get("http://192.168.43.230/api_pariwisata/berita.php").then(res => {
+            this.setState({
+                wisata: res.data.data
+            })
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    keyExtractor = (item, index) => index.toString()
+    renderItem = ({ item }) => (
+        <NewsItem nama={item.judul_berita} foto={{ uri: this.state.prefik_url + item.foto1 }} />
     )
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <FlatList
+                    keyExtractor={this.keyExtractor}
+                    data={this.state.wisata}
+                    renderItem={this.renderItem}
+                />
+            </View>
+        )
+    }
+
 }
 
 export default NewsBar
